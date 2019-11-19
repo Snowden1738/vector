dfe00ac286d5104d156109f1f69d50b98def1986
Arkajyoti Banerjee <34052538+Snowden1738@users.noreply.github.com>
2019-11-19 23:27:19 +0530

    Defined general allocator functions
    Defined `reserve`, `resize`, `vector_new`, `access`.

--- vector.h
+++ vector.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct _vector
 {
@@ -19,3 +19,74 @@ int* insert (vector*, int);
 int* erase (vector*, int);
 int* push_back (vector* v, int);
 int* pop_back (vector* v, int);
+
+int capacity_bound (int size)
+{
+	int bound = 1;
+
+	while (bound < size)
+		bound <<= 1;
+
+	return bound;
+}
+
+int reserve (vector* v, int capacity)
+{
+	if (capacity > v -> capacity)
+	{
+		int temp_capacity = capacity_bound (capacity);
+		int* temp = (int*) realloc (v -> base_addr, sizeof (int) * temp_capacity);
+
+		if (temp)
+		{
+			v -> base_addr = temp;
+			v -> capacity = temp_capacity;
+
+			return 0;
+		}
+		else
+			return INT_MAX;
+	}
+	else
+		return 0;
+}
+
+int resize (vector* v, int size)
+{
+	if (size == v -> size)
+		return 0;
+	else if (size < v -> size)
+	{
+		v -> base_addr = (int*) realloc (v -> base_addr, sizeof (int) * (v -> capacity = capacity_bound (size)));
+		v -> size = size;
+
+		return 0;
+	}
+	else
+	{
+		if (!reserve (v, size))
+		{
+			v -> size = size;
+
+			return 0;
+		}
+		else
+			return INT_MAX;
+	}
+}
+
+vector vector_new (int size)
+{
+	vector v = {(int*) calloc (1, sizeof (int)), 1, 1};
+	resize (&v, size);
+
+	return v;
+}
+
+int* access (vector v, int idx)
+{
+	if (idx < v.size)
+		return v.base_addr + idx;
+	else
+		return NULL;
+}
